e3bf55b2b300a14f700f6b7757c8838812875dda
Mangaliso Kali <your_email@example.com>
2026-02-23 17:24:04 +0000

    Add azurerm backend with OIDC for dev

---
+++ envs/dev/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstate27175"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+
+    use_oidc        = true
+    client_id       = "61baab45-4089-4552-aaae-6690d326c5fd"
+    tenant_id       = "c64cd139-e40e-4967-bc0b-1a72021890af"
+    subscription_id = "80a31f80-0116-4fe0-acf5-ccaae10138a8"
+  }
+}
