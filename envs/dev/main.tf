ee655b90322c274124595566d34ab4f73d595b30
Mangaliso Kali <kalimangaliso1@gmail.com>
2026-02-23 15:20:58 +0200

    Configure remote backend for dev (Azure Storage)

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstate27175"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+terraform {
   required_version = ">= 1.5.0"
   required_providers {
     azurerm = {
